0b716ae84dca15b4630e402d66391600cd744fa4
davidjouellette <21369527+davidjouellette@users.noreply.github.com>
2018-09-14 09:51:39 -0400

    Bug Fixes
    Fixed input syntax of Popen calls, converted inputs from ghost_config.txt to integers, added a call to haunt so that the script can be run.

--- ghost_raspberry.py
+++ ghost_raspberry.py
@@ -32,14 +32,15 @@ def configure():
     global playlistLoop
     for line in configLines:
         if "maximum_time" in line:
-            hauntIntervalMax = re.findall(r"\d+",line)[0]
+            hauntIntervalMax = int(re.findall(r"\d+",line)[0])
         if "minimum_time" in line:
-            hauntIntervalMin = re.findall(r"\d+",line)[0]
+            hauntIntervalMin = int(re.findall(r"\d+",line)[0])
         if "activation_mode" in line:
-            hauntMode = re.findall(r"\d+",line)[0]
+            hauntMode = int(re.findall(r"\d+",line)[0])
         if "playlist_Mode" in line:
-            playlistLoop = re.findall(r"\d+",line)[0]
+            playlistLoop = int(re.findall(r"\d+",line)[0])
         if "total_duration" in line:
+            duration = int(re.findall(r"\d+",line)[0])
     if playlistMode !=2:
         random.shuffle(sounds)
 # Here we use the Raspberry Pi's onboard omxplayer to deal with any number of
@@ -53,14 +54,14 @@ def configure():
 def randomNoise():
     global sounds
     if playlistMode == 0:
-        Popen("omxplayer", sounds[random.randint(0,len(sounds)-1)])
+        Popen(["omxplayer", sounds[random.randint(0,len(sounds)-1)]])
     if playlistMode == 1 or playlistMode == 2:
         global playlistIndex
         if playlistIndex == len(sounds):
             playlistIndex = 0
             if playlistMode == 1:
                 random.shuffle(sounds)
-        Popen("omxplayer", sounds[playlistIndex])
+        Popen(["omxplayer", sounds[playlistIndex]])
 
 # Trigger an electrical relay with the pi's GPIO pins. For now we just have one
 # output but we could add more, and even randomize which gets triggered.
@@ -99,6 +100,4 @@ def haunt(tMin,tMax):
         if duration > 0:
             if time.time() - timer_start >= duration:
                 running = 0
-                
-                
-    
+haunt(hauntIntervalMin,hauntIntervalMax)
